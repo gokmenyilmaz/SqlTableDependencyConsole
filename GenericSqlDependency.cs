f747b60e4b0283cc55c10cb47621dec4e70b713e
GökmenYilmaz <gokmen1977@msn.com>
2019-03-23 22:12:37 +0300

    Add project files.

---
+++ GenericSqlDependency.cs
@@ -0,0 +1,65 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.Delegates;
+using TableDependency.SqlClient.Base.Enums;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace Pandap.UI.Modules.OperasyonModule
+{
+    public class HaftalikPlanDTO
+    {
+        public string Id { get; set; }
+        public string Pazartesi { get; set; }
+        public string Sali { get; set; }
+        public string Carsamba { get; set; }
+        public string Persembe { get; set; }
+        public string Cuma { get; set; }
+        public string Cumartesi { get; set; }
+        public string Pazar { get; set; }
+
+    }
+
+    public class GenericSqlDependency
+    {
+        public static GenericSqlDependency Default { get; set; }
+
+        SqlTableDependency<HaftalikPlanDTO> tdp;
+
+        static GenericSqlDependency()
+        {
+            Default = new GenericSqlDependency();
+        }
+
+        public void Basla()
+        {
+            string cnnStr = $"server=localhost,1501;database=PANDAPDB;user id=sa;password=0000;App=Pandap";
+            tdp = new SqlTableDependency<HaftalikPlanDTO>(cnnStr, "HaftalikYuklemePlan", "Operasyon");
+            tdp.OnChanged += TableDependency_Changed;
+            tdp.Start();
+        }
+
+        private void TableDependency_Changed(object sender, RecordChangedEventArgs<HaftalikPlanDTO> e)
+        {
+            if (e.ChangeType != ChangeType.None)
+            {
+                Messenger.Default.Send(e.Entity);
+
+               
+            }
+        }
+
+
+        public void Durdur()
+        {
+            tdp.OnChanged -= TableDependency_Changed;
+            tdp.Stop();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.EventArgs;
+
+public class HaftalikPlan
+{
+    public string Id { get; set; }
+    public string Pazartesi { get; set; }
+}
+
+public class Program
+{
+    private static string _con = $"server=localhost,1501;database=PANDAPDB;user id=sa;password=0000;App=Pandap";
+
+    public static void Main()
+    {
+
+        using (var dep = new SqlTableDependency<HaftalikPlan>(_con, "HaftalikYuklemePlan","Operasyon")) 
+        {
+            dep.OnChanged += Changed;
+            dep.Start();
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+
+            dep.Stop();
+        }
+    }
+
+    public static void Changed(object sender, RecordChangedEventArgs<HaftalikPlan> e)
+    {
+        var changedEntity = e.Entity;
+
+        Console.WriteLine("DML operation: " + e.ChangeType);
+        Console.WriteLine("ID: " + changedEntity.Id);
+      
+    }
+}
